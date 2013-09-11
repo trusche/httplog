39b8666b38862248feb42dbce30a82276801b735
Andrew Hammond <andrew@evertrue.com>
2013-09-11 16:11:23 -0400

    Add httpclient adapter support for whitelist/blacklist patterns

--- lib/httplog/adapters/httpclient.rb
+++ lib/httplog/adapters/httpclient.rb
@@ -4,9 +4,13 @@ if defined?(::HTTPClient)
     alias_method :orig_do_get_block, :do_get_block
 
     def do_get_block(req, proxy, conn, &block)
-      HttpLog.log_request(req.header.request_method, req.header.request_uri)
-      HttpLog.log_headers(req.headers)
-      HttpLog.log_data(req.body) if req.header.request_method == "POST"
+      log_enabled = HttpLog.url_approved?(req.header.request_uri)
+
+      if log_enabled
+        HttpLog.log_request(req.header.request_method, req.header.request_uri)
+        HttpLog.log_headers(req.headers)
+        HttpLog.log_data(req.body) if req.header.request_method == "POST"
+      end
 
       retryable_response = nil
       bm = Benchmark.realtime do
@@ -17,12 +21,14 @@ if defined?(::HTTPClient)
         end
       end
 
-      res = conn.pop
-      HttpLog.log_compact(req.header.request_method, req.header.request_uri, res.status_code, bm)
-      HttpLog.log_status(res.status_code)
-      HttpLog.log_benchmark(bm)
-      HttpLog.log_body(res.body)
-      conn.push(res)
+      if log_enabled
+        res = conn.pop
+        HttpLog.log_compact(req.header.request_method, req.header.request_uri, res.status_code, bm)
+        HttpLog.log_status(res.status_code)
+        HttpLog.log_benchmark(bm)
+        HttpLog.log_body(res.body)
+        conn.push(res)
+      end
 
       raise retryable_response if retryable_response != nil
     end
@@ -30,7 +36,9 @@ if defined?(::HTTPClient)
     class Session
       alias_method :orig_create_socket, :create_socket
       def create_socket(site)
-        HttpLog.log_connection(site.host, site.port)
+        if HttpLog.url_approved?("#{site.host}:#{site.port}")
+          HttpLog.log_connection(site.host, site.port)
+        end
         orig_create_socket(site)
       end
     end
