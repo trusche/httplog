c3f12c4431a38be93364abfde391f78d1a036070
Mikael Henriksson <mikael@zoolutions.se>
2016-03-08 11:36:18 +0100

    Make httplog work with newer httprb/http

--- lib/httplog/adapters/http.rb
+++ lib/httplog/adapters/http.rb
@@ -1,7 +1,11 @@
 if defined?(::HTTP::Client) and defined?(::HTTP::Connection)
   module ::HTTP
     class Client
-      alias_method(:orig_make_request, :make_request) unless method_defined?(:orig_make_request)
+      if respond_to?(:make_request)
+        alias_method(:orig_make_request, :make_request) unless method_defined?(:orig_make_request)
+      else
+        alias_method(:orig_perform, :perform) unless method_defined?(:orig_perform)
+      end
 
       def make_request(req, options)
 
@@ -29,15 +33,39 @@ if defined?(::HTTP::Client) and defined?(::HTTP::Connection)
         @response
       end
 
+      def perform(req, options)
+
+        log_enabled = HttpLog.url_approved?(req.uri)
+
+        if log_enabled
+          HttpLog.log_request(req.verb, req.uri)
+          HttpLog.log_headers(req.headers.to_h)
+          HttpLog.log_data(req.body) #if req.verb == :post
+        end
+
+        bm = Benchmark.realtime do
+          @response = orig_perform(req, options)
+        end
+
+        if log_enabled
+          headers = @response.headers
+          HttpLog.log_compact(req.verb, req.uri, @response.code, bm)
+          HttpLog.log_status(@response.code)
+          HttpLog.log_benchmark(bm)
+          HttpLog.log_headers(@response.headers.to_h)
+          HttpLog.log_body(@response.body, headers['Content-Encoding'], headers['Content-Type'])
+        end
+
+        @response
+      end
+
     end
 
     class Connection
       alias_method(:orig_initialize, :initialize) unless method_defined?(:orig_initialize)
 
       def initialize(req, options)
-
         HttpLog.log_connection(req.uri.host, req.uri.port) if HttpLog.url_approved?(req.uri)
-
         orig_initialize(req, options)
       end
     end
