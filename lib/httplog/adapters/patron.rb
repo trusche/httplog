5a110823b89463a697838b395a4ff6ea378c2dd1
Manuel Bustillo <manuel.bustillo@coverwallet.com>
2018-11-06 12:57:10 +0100

    Implement JSON logging for Patron

--- lib/httplog/adapters/patron.rb
+++ lib/httplog/adapters/patron.rb
@@ -20,6 +20,16 @@ if defined?(Patron)
         if log_enabled
           headers = @response.headers
           HttpLog.log_compact(action_name, url, @response.status, bm)
+          HttpLog.log_json(
+            method: action_name,
+            url: url,
+            request_body: options[:data],
+            request_headers: headers,
+            response_code: @response.status,
+            response_body: @response.body,
+            response_headers: headers,
+            benchmark: bm
+          )
           HttpLog.log_status(@response.status)
           HttpLog.log_benchmark(bm)
           HttpLog.log_headers(headers)
