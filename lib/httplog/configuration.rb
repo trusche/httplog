a3324e1191cde0842f0c4b846d66aa2c005cd0b2
Vladimir Kitaev <vkitaev@freeconferencecall.com>
2020-01-16 14:39:05 +0300

    Rework Graylog support. Allow different Graylog log formats.

--- lib/httplog/configuration.rb
+++ lib/httplog/configuration.rb
@@ -5,7 +5,7 @@ module HttpLog
     attr_accessor :enabled,
                   :compact_log,
                   :json_log,
-                  :graylog,
+                  :graylog_formatter,
                   :logger,
                   :logger_method,
                   :severity,
@@ -31,7 +31,7 @@ module HttpLog
       @enabled                 = true
       @compact_log             = false
       @json_log                = false
-      @graylog                 = false
+      @graylog_formatter       = nil
       @logger                  = Logger.new($stdout)
       @logger_method           = :log
       @severity                = Logger::Severity::DEBUG
