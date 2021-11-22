e7e89f0ea075c47c6900760845f22163fe18ad7f
Thilo Rusche <thilorusche@gmail.com>
2021-11-22 11:21:41 +0000

    Revert "Fix for https://github.com/trusche/httplog/pull/107"
    This reverts commit 9f6e8a70c4d39bc91e33c5f0d03de152f0f52734.

--- lib/httplog/http_log.rb
+++ lib/httplog/http_log.rb
@@ -3,12 +3,8 @@
 require 'net/http'
 require 'logger'
 require 'benchmark'
+require 'rainbow'
 require 'rack'
-begin
-  require 'rainbow'
-rescue LoadError
-  # Ignored
-end
 
 module HttpLog
   LOG_PREFIX = '[httplog] '.freeze
